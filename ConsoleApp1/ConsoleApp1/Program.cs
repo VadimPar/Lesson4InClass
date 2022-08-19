0f2b5d50d75da451aab001b665a9094a66830717
Вадим Паращенко <VadimP@live.ru>
2022-08-19 19:35:51 +0300

    task 1,2

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿double s = 200;
+for(int i = 0; i < 8; i++)
+{
+    s *= 1.1;
+}
+Console.WriteLine(s);   
+
+
+for(int i = 0; i <= 200; i++)
+{
+    if (i % 8 == 0)
+    {
+        Console.Write(i+"\t");
+    }
+}
